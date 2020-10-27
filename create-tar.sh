f60b3c816751dba03e3ca5b086e9a49c3de7b123
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-10-27 20:00:07 +0530

    initial commit

---
+++ create-tar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+jq \
+    --arg pid $(jq -r ".type" input.json | fold -1 | head -n 1)$(echo $GITHUB_SHA | fold -w 7 | head -n 1) \
+    '. | {pid: $pid, name: .name, version: .version, fields: .fields, type: .type, runner_script: .runner_script}' \
+    input.json > data.json
+NAME=$(jq -r ".name" input.json)
+export TARNAME=$NAME\_$(jq -r ".version" input.json).tar.gz
+echo "TARNAME=$TARNAME" >> $GITHUB_ENV
+tar -czvf $TARNAME data.json $NAME
